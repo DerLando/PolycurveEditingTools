9d5b34f2ee91651f822ed1ed96237baaae3f5745
Lando <lando.schumpich@gmail.com>
2019-11-05 13:27:26 +0100

    Added EditGripsEnabler static class

---
+++ PolycurveEditingTools/Core/PolycurveEditGripsEnabler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+
+namespace PolycurveEditingTools.Core
+{
+    internal static class PolycurveEditGripsEnabler
+    {
+        public static void TurnOnGrips(RhinoObject rhObject)
+        {
+            // return on null input
+            if (rhObject == null) return;
+
+            // try to cast input geometry to polycurve
+            var polyCurve = rhObject.Geometry as PolyCurve;
+            if (polyCurve == null) return;
+
+            var polycurveEditGrips = new PolycurveEditGrips();
+            if(!polycurveEditGrips.CreateGrips(polyCurve)) return;
+
+            rhObject.EnableCustomGrips(polycurveEditGrips);
+        }
+    }
+}
