bd2f127ce195997e0f5588e686e3d31e0f8f7421
Lando <lando.schumpich@gmail.com>
2019-11-05 13:18:46 +0100

    Initial structure, added Polycurve edit grip

---
+++ PolycurveEditingTools/Core/PolycurveEditGrip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.DocObjects.Custom;
+
+namespace PolycurveEditingTools.Core
+{
+    public class PolycurveEditGrip : CustomGripObject
+    {
+        public bool IsActive { get; set; }
+
+        public PolycurveEditGrip()
+        {
+            IsActive = true;
+        }
+
+        public override string ShortDescription(bool plural)
+        {
+            return plural ? "Polycurve edit points" : "Polycurve edit point";
+        }
+    }
+}
